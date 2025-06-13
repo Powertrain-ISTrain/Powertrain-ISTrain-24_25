4cb818e5130c852d48564fa883b5bcfc9d7d1659
miguelmeninas2003 <istrainpowertrain@gmail.com>
2025-06-13 19:46:07 +0100

    sizing
    sizing

--- Calculations/SH Calculations/Performance_Calculations_Main_script.m
+++ Calculations/SH Calculations/Performance_Calculations_Main_script.m
@@ -19,9 +19,9 @@ This script is made with the following ASSUMPTIONS:
 %Note: Fixed parameters are based on the Technical Specifications 2025 by
 %IMech
 
-%%
+%% Parameters
 
-%fixed Parameters
+% Fixed Parameters
 
 max_velocity=4.17; %m/s
 adhesion_coeff= 0.15:0.05:0.6; % Lower and Upper bounds of the friction coefficient betwenn the wheels and rails
@@ -39,34 +39,11 @@ driven_wheels=8; %
 nr_motors=2;% one motor per bogie
 transmission_eff=0.85;%Average value
 wheel_radius=0.100; %m - Minimum wheel diameter is 200 mm
-%%
-
-%% Parameters
-
-% Fixed Parameters
-
-max_velocity=4.17; %m/s
-wheel_radius=0.100; %m - Minimum wheel diameter is 200 mm
-adhesion_coeff= 0.15:0.05:0.6; % Lower and Upper bounds of the friction coefficient betwenn the wheels and rails
-loco_mass=2000 ; % kg , estimate
-max_trailing_mass=1800; %kg, specified in technical specifications
-driven_wheels=8;
-nr_motors=2;% one motor per bogie
-Rolling_coeff=0.004;%Rolling Ressístance coeff
-g=9.81; %m/s^2
-transmission_eff=0.85;%Average value
-Aerodynamic_drag=5.1; % N
-max_gradient=2; % 
-
-%Variable Parameters
-
-G=5; %Gear Ratio between the axle and the motor  
-gear_ratio=1/5; % gear ratio (w_motor/w_axle)
 
 %% RESULTS
 [f_motive, f_wheel] = forces(max_gradient, loco_mass,max_trailing_mass, g, driven_wheels, ...
     adhesion_coeff, Rolling_coeff, Aerodynamic_drag);
-%%
+
 [max_accel_loaded,max_accel_unloaded,time_15kmh] = accelerations(f_motive,adhesion_coeff,loco_mass,max_trailing_mass, max_velocity);
-%%
+
 [motor_torque,max_motor_speed,max_motor_power] = MotorParameters(f_wheel,wheel_radius,transmission_eff,G,adhesion_coeff,max_velocity);
--- Calculations/peak_motor_calc.m
+++ Calculations/peak_motor_calc.m
@@ -2,16 +2,16 @@
 % Given parameters
 v_max    = 4.17;        % m/s
 r_w      = 0.100;       % m
-m_loco   = 450;         % kg
+m_loco   = 350;         % kg
 m_trail  = 1800;        % kg
 Crr      = 0.004;       % rolling resistance
 g        = 9.81;        % m/s^2
 grade    = 0.02;        % 2% incline
-G        = 8;           % gearbox ratio
-eta_mech = 0.90;        % mech efficiency
-eta_elec = 0.85;        % elec efficiency
+G        = 5;           % gearbox ratio
+eta_mech = 0.85;        % mech efficiency
+eta_elec = 0.90;        % elec efficiency
 n_mot    = 2;           % number of motors
-a=1;
+a=0.35;
 %adicionar tempo
 
 %% 1) Total mass
@@ -28,7 +28,7 @@ T_wheel = F_req * r_w;
 
 %% 4) Motor torque (per motor) for start
 T_motor = (T_wheel / n_mot) / (G * eta_mech);
-T_motor = 1.15 * T_motor;       % +15% margin
+%T_motor = 1.15 * T_motor;       % +15% margin
 
 %% 5) Motor speed at v_max
 omega_wheel = v_max / r_w;           
@@ -37,7 +37,7 @@ rpm_motor   = omega_motor * 60/(2*pi);
 
 %% 6) Mechanical power at wheels & apply safety margin
 P_mech_wheel = F_req * v_max;        % P = F * v
-P_mech_wheel = 1.15 * P_mech_wheel;  % +15% margin
+%P_mech_wheel = 1.15 * P_mech_wheel;  % +15% margin
 
 %% 7) Mechanical power at motor shaft (total & per‑motor)
 P_mech_motor_total = P_mech_wheel / eta_mech;
